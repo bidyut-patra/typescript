b0968d2616bf087e504f6eee6d7fc0bc37b7e108
Bidyut Kumar Patra <SESA503898@schneider-electric.com>
2020-04-18 14:37:28 +0530

    compose components

--- react-vault/src/personal-details/personalDetails.js
+++ react-vault/src/personal-details/personalDetails.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import './personalDetails.css';
 
 export class PersonalDetails extends Component {
     render() {
+        let style = {
+            marginRight: 0,
+            padding: 20
+        }
+
         return (
-            <div>Personal details...</div>
+            <div className="row personal-details-content" style={style}>
+                <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    <label htmlFor="action">Apt Number*</label>
+                    <select className="form-control custom-font">
+                        <option></option>
+                        <option>
+                            TEXT
+                        </option>
+                    </select>
+                </div>
+                <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    <label htmlFor="owner">Owner's Name</label>
+                    <input className="form-control custom-font" readOnly id="owner"/>
+                </div>          
+            </div>
         )
     }
 }
--- react-vault/src/save-personal-data/savePersonalData.js
+++ react-vault/src/save-personal-data/savePersonalData.js
@@ -1,9 +1,70 @@
 import React, { Component } from 'react';
+import './savePersonalData.css'
+import { PersonalDetails } from '../personal-details/personalDetails';
 
 export class SavePersonalData extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            rows: []
+        };
+
+        // This binding is required to access member variables and in-built 'state' member
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onAdd = this.onAdd.bind(this);
+    }
+
+    onSubmit = () => {
+
+    }
+
+    onAdd = () => {
+        let eRows = this.state.rows;
+        eRows.push({
+            id: 'row_' + eRows.length
+        });
+        this.setState({
+            rows: eRows
+        });
+    }
+
+    onRemove = (rowId) => {
+        let eRows = this.state.rows;
+        let rowIndex = eRows.findIndex(r => r.id === rowId);
+        if (rowIndex >= 0) {
+            eRows.splice(rowIndex, 1);
+            this.setState({
+                rows: eRows
+            });
+        }
+    }
+
     render() {
+        let personalDetails = [];
+        let saveBtn;
+
+        if (this.state.rows.length > 0) {
+            for (let rIndex = 0; rIndex < this.state.rows.length; rIndex++) {
+                let rowId = this.state.rows[rIndex].id;
+                let removeBtn = <button className="btn btn-secondary" onClick={() => this.onRemove(rowId)}>X</button>;
+                let personalHeader = <div className="personal-data-header">{removeBtn}</div>;
+                let personalContent = <div className="personal-details"><PersonalDetails></PersonalDetails></div>;
+                personalDetails.push(<div key={rIndex}>{personalHeader}{personalContent}</div>);
+            }
+
+            personalDetails = <div className="list-of-personal-details">{personalDetails}</div>;
+            saveBtn = <button className="fixed-bottom btn btn-primary btn-save" type="submit" onClick={this.onSubmit}>Save</button>;
+        }
+
+        let addBtn = <button className="btn btn-primary btn-add" onClick={this.onAdd}>Add</button>;
+
         return (
-            <div>Save personal data page...</div>
+            <div>
+                {addBtn}
+                {personalDetails}
+                {saveBtn}
+            </div>
         )
     }
 }
